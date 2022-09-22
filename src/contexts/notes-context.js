6d1b4ecd1bc795c827f3aa95130c6f439946b599
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2022-09-22 15:17:49 +0530

    Update notes-context.js

--- src/contexts/notes-context.js
+++ src/contexts/notes-context.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { getArchivesService } from "../services/archive-services/getArchivesService";
 import { getNotesService } from "../services/note-services/getNotesService";
